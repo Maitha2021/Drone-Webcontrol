dd6179dd7374d6a370bf5c28be1f850726536c45
Maitha  Al suboosi <201607005@Maithas-MacBook-Pro-57.local>
2021-02-18 11:18:57 +0400

    first commit

---
+++ mypjt.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/about')
+def goodbye_world():
+    return 'Goodbye,World!'
